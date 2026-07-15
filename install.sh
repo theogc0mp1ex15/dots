1e5a1948b73c61194cf0687b0c21a839b218c9fb
theogc0mp1ex15 <theogc0mp1ex15@gmail.com>
2026-07-15 22:00:57 +0530

    alacritty fix

--- install.sh
+++ install.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
 
-set -e  # stop the script if any command fails
+set -e # stop the script if any command fails
 
 echo "==> Installing official repo packages..."
 
 sudo pacman -S --needed --noconfirm \
-    stow \
-    alacritty \
-    kitty \
-    neovim \
-    btop \
-    flameshot \
-    rofi \
-    niri \
-    vlc \
-    gparted \
-    nautilus
+  stow \
+  alacritty \
+  kitty \
+  neovim \
+  btop \
+  flameshot \
+  rofi \
+  niri \
+  vlc \
+  gparted \
+  zen-browser-bin \
+  brave-bin \
+  paru \
+  yay \
+  nautilus
 
 echo "==> Installing AUR packages..."
 
-paru -S --needed --noconfirm \
-    zen-browser-bin \
-    brave-bin
+#paru -S --needed --noconfirm \
+#//etc/zen-browser-bin \
+#brave-bin
 
 # Uncomment and adjust if noctalia is AUR-only on your system
 # paru -S --needed --noconfirm noctalia
